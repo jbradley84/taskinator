eb1c2387ac6da57661861e07173b696e42b35d2d
jbradley84 <utbradley84@yahoo.com>
2022-05-22 13:39:18 -0500

    added ability to store task data into localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,12 +56,10 @@ var createTaskEl = function (taskDataObj) {
 
     taskDataObj.id = taskIdCounter;
     tasks.push(taskDataObj);
+    saveTasks();
 
     // increase task counter for next unique id
     taskIdCounter++;
-
-    console.log(taskDataObj);
-    console.log(taskDataObj.status);
 };
 
 var createTaskActions = function (taskId) {
@@ -119,6 +117,8 @@ var completeEditTask = function (taskName, taskType, taskId) {
         }
     };
 
+    saveTasks();
+
     alert("Task Updated!");
 
     // remove data attribute from form
@@ -143,7 +143,6 @@ var taskButtonHandler = function (event) {
 };
 
 var taskStatusChangeHandler = function (event) {
-    console.log(event.target.value);
 
     // find task list item based on event.target's data-task-id attribute
     var taskId = event.target.getAttribute("data-task-id");
@@ -167,7 +166,8 @@ var taskStatusChangeHandler = function (event) {
             tasks[i].status = statusValue;
         }
     }
-    console.log(tasks);
+    
+    saveTasks();
 };
 
 var editTask = function (taskId) {
@@ -211,8 +211,15 @@ var deleteTask = function (taskId) {
     }
 
     // reassign tasks array to be the same as updatedTaskArr
-    tasks = updatedTaskArr;
+    tasks = updateTaskArr;
+
+    saveTasks();
 };
+
+// function to save tasks to localStorage
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
